902a89d3f72f01e8c6d776956d2370f569d2389f
Bruce MacKenzie <mackenzbb@gmail.com>
2020-10-19 16:25:12 -0400

    Logging changes

--- Windows/disable_wake.ps1
+++ Windows/disable_wake.ps1
@@ -4,7 +4,7 @@ existing devices from waking computer by default.
 
 To configure to run automatically:
     $trigger = New-JobTrigger -AtStartup -RandomDelay 00:00:30
-    Register-ScheduledJob -Trigger $trigger -FilePath C:\Users\BMacK\GitHub\scripts\Windows\disable_wake.ps1 -Name DisableDeviceWake
+    Register-ScheduledJob -Trigger $trigger -FilePath C:\Users\BMacK\repos\scripts\Windows\disable_wake.ps1 -Name DisableDeviceWake
 
 To view the scheduled job or trigger:
     Get-ScheduledJob DisableDeviceWake
@@ -12,25 +12,31 @@ To view the scheduled job or trigger:
 
 Test the script by manually enabling a device to wake, e.g.,
     powercfg /deviceenablewake "HID Keyboard Device (006)"
+
+Logs:
+    C:\Users\BMacK\AppData\Local\Microsoft\Windows\PowerShell\ScheduledJobs\DisableDeviceWake
 #>
 
 Set-ExecutionPolicy RemoteSigned
 
+# Write-Output "Running DisableDeviceWake" | Out-File -FilePath C:\Users\BMacK\DisableDeviceWake.log
+
 $devices=powershell -Command "powercfg /devicequery wake_armed"
 if ($devices -eq "NONE") {
+    Write-Output "No devices to disable. Exiting."
     exit
 }
 
 foreach ($d in $devices) {
     if ($d -ne "") {
-        Write-Host Disabling: $d
+        Write-Output "Disabling: $d"
         powercfg /devicedisablewake $d
     }
 }
 
 if ("NONE" -ne $(powershell -Command "powercfg /devicequery wake_armed")) {
-    Write-Host "Failed to disable all devices"
+    Write-Output "Failed to disable all devices"
 }
 else {
-    Write-Host "All devices disabled from waking computer"
+    Write-Output "All devices disabled from waking computer"
 }
